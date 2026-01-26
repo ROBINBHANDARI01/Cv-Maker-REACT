7ef02e75e575c345a4f04bdda8a456aaf0421694
Robin Singh Bhandari <robinbhandari202@gmail.com>
2026-01-26 23:12:49 +0530

    Added a project route

--- src/Components/form/navbar.jsx
+++ src/Components/form/navbar.jsx
@@ -3,10 +3,15 @@ import "./nav.css"
 function Navbar(){
     return(
         <div className="Nav-Bar">
-            <NavLink to="/general">General</NavLink>
-            <NavLink to="/about">About</NavLink>
-            <NavLink to="/experience">Experience</NavLink>
-            <NavLink to="/skills">Skills</NavLink>
+            <NavLink to="/general"><p className="navBtn">General</p></NavLink>
+            <NavLink to="/about"><p className="navBtn">About</p></NavLink>
+            <NavLink to="/experience"><p className="navBtn">Experience</p></NavLink>
+            <NavLink to="/skills"><p className="navBtn">Skills</p></NavLink>
+            <NavLink to="/projects"><p className="navBtn">Projects</p></NavLink>
+            <button
+            className="print"
+          onClick={()=>window.print()}
+          >Print</button>
         </div>
     )
 }
