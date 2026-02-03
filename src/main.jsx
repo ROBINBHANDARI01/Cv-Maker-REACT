d0fa8ba10e789106e0c8b58ba4e07be3ad4275dd
Robin Singh Bhandari <robinbhandari202@gmail.com>
2026-02-03 21:53:50 +0530

    changed some default values

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Router } from 'react-router-dom'
+import { BrowserRouter} from 'react-router-dom'
 import { ResumeProvider } from './context/ResumeContext.jsx'
 
 import App from './App.jsx'
