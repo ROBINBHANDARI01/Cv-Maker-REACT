77fc4dd8b4bbe3858c7e19094b106e9a9ec32a49
Robin Singh Bhandari <robinbhandari202@gmail.com>
2026-03-31 23:52:36 +0530

    fixed some styling issues in the home page

--- src/Components/home/homeNav.jsx
+++ src/Components/home/homeNav.jsx
@@ -3,11 +3,11 @@ import { useNavigate } from "react-router-dom";
 export default function HomeNav(){
     const navigate = useNavigate();
     return(
-        <nav class="flex justify-between mx-7 my-4 sm:mx-17 lg:mx-60">
+        <nav class="flex justify-between mx-7 my-4 sm:mx-17 lg:mx-70">
 
-        <img onClick={()=> navigate("/")} src={logo} alt="logo" class="h-7 cursor-pointer"></img>
+        <img onClick={()=> navigate("/")} src={logo} alt="logo" class="h-7 cursor-pointer md:h-9"></img>
         <ul>
-            <li><a onClick={()=> navigate("/navigation")} href="#" class="font-inter">Get Started</a></li>
+            <li><a onClick={()=> navigate("/navigation")} href="#" class="text-[0.9rem] font-medium text-blue-700 hover:bg-blue-100 hover:text-blue-900 hover:border-blue-500 font-inter border p-2  border-blue-200 rounded-md ">Get Started</a></li>
         </ul>
         </nav>
     )
--- src/Components/home/homepage.jsx
+++ src/Components/home/homepage.jsx
@@ -11,19 +11,29 @@ function Homepage() {
           Get Your Dream Job
         </h1></div>
         <div>
-        <h1 class="font-inter text-3xl mb-1 lg:text-3xl">With Our Resume Maker</h1></div>
+        <h1 class="font-inter text-3xl font-medium mb-1 lg:text-3xl">With Our Resume Maker</h1></div>
       </div>
-      <div class="mt-4">
-       <p class="font-Inter text-center text-[13px] mx-10 md:text-[14px]">Get your resume noticed with our intutive builder. Designed to help you build a professional profile that gets result.</p>
+      <div class="mt-4 flex justify-center ">
+       <p class="font-Inter text-center text-[13px] mx-10 md:text-[14px] ">Get your resume noticed with our intutive builder Designed to help you build a professional profile that gets result.</p>
       </div>
       <div class="flex justify-center mt-8">
         <button onClick={()=> navigate("/navigation")} class="bg-blue-400 rounded-xl px-5 py-1.5  text-white cursor-pointer">Create Resume</button>
       </div>
 
-      <div className="bg-blue-400 h-99 mt-6 p-2">
+
+      <div>
+        <div class="h-140 bg-blue-200 m-9 rounded-2xl w-xl">
+
+        </div>
+        <div>
+        <h1>Let’s build your free, job-winning resume in 3 simple steps</h1>
+      </div>
+      </div>
+
+      <div className="bg-linear-to-r from-[#21b1c1] via-[#1d80ea] to-[#1d8fbc] h-[80vh] mt-6 p-2">
         
         <h2 className="text-center text-[20px] font-bold font-Inter  text-white ">Choose between many templates </h2>
-        <p className="text-sm text-white font-extralight text-center mt-2 mx-10">Land more interviews with a professional edge. Our Resume Builder helps you create a standout CV in minutes. </p>
+        <p className="text-sm text-white font-extralight text-center mt-2 mx-10">Land more interviews with a professional edge. Our Resume Builder helps you create a standout resumes in minutes. </p>
   
      </div>
     </>
