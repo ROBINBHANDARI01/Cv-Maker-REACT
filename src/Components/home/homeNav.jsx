3311824d4515d7d568e2866a63851a900b167c89
Robin Singh Bhandari <robinbhandari202@gmail.com>
2026-04-02 13:54:41 +0530

    minor nav bar adjustment

--- src/Components/home/homeNav.jsx
+++ src/Components/home/homeNav.jsx
@@ -5,9 +5,9 @@ export default function HomeNav(){
     return(
         <nav class="flex justify-between mx-7 my-4 sm:mx-17 lg:mx-70">
 
-        <img onClick={()=> navigate("/")} src={logo} alt="logo" class="h-7 cursor-pointer md:h-9"></img>
+        <img onClick={()=> navigate("/")} src={logo} alt="logo" class="h-7 cursor-pointer md:h-10"></img>
         <ul>
-            <li><a onClick={()=> navigate("/navigation")} href="#" class="text-[0.9rem] font-medium text-blue-700 hover:bg-blue-100 hover:text-blue-900 hover:border-blue-500 font-inter border p-2  border-blue-200 rounded-md ">Get Started</a></li>
+            <li><a onClick={()=> navigate("/navigation")} href="#" class="text-[0.9rem] font-medium text-blue-700 hover:bg-blue-100 hover:text-blue-900 hover:border-blue-500 font-inter border p-2  border-blue-200 rounded-md lg:text-lg">Get Started</a></li>
         </ul>
         </nav>
     )
