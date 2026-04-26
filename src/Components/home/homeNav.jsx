580fbd34c23eda58bcc4a2f23a2ce9b765184084
Robin Singh Bhandari <robinbhandari202@gmail.com>
2026-04-27 03:04:33 +0530

    created a new folder "Home-component" to make the home page component based.

--- src/Components/home/homeNav.jsx
+++ src/Components/home/homeNav.jsx
@@ -6,9 +6,10 @@ export default function HomeNav(){
         <nav class="flex justify-between mx-7 my-4 sm:mx-15 lg:mx-70">
 
         <img onClick={()=> navigate("/")} src={logo} alt="logo" class="h-5 cursor-pointer md:h-9"></img>
-        <ul>
-            <li><a onClick={()=> navigate("/navigation")} href="#" class="text-[0.7rem] font-medium text-blue-700 hover:bg-blue-100 hover:text-blue-900 hover:border-blue-500 font-inter border p-2  border-blue-200 rounded-md lg:text-[1rem]">Get Started</a></li>
-        </ul>
+        <div >
+            <a  onClick={()=> navigate("/navigation")} href="#" class="text-[0.7rem] font-medium text-blue-700 hover:bg-blue-100   font-inter border p-2  border-blue-200 rounded-md lg:text-sm">Login</a>
+            <a onClick={()=> navigate("/navigation")} href="#" class="ml-1 text-[0.7rem] font-medium bg-(--color-primary) text-white hover:bg-blue-100 hover:text-blue-900 hover:border-blue-500 font-inter border p-2 rounded-md lg:text-sm">Get Started</a>
+        </div>
         </nav>
     )
 }
--- src/Components/home/homepage.jsx
+++ src/Components/home/homepage.jsx
@@ -1,135 +1,25 @@
-import { useNavigate } from "react-router-dom";
+
+
+import Hero from "../homeComponents/Hero"
+import Steps from "../homeComponents/Steps";
 
 import TemplateSlider from "./templateSlider";
-import temp7 from "../../assets/template7.webp";
-import temp19 from "../../assets/template19.webp";
-import temp18 from "../../assets/template18.webp";
 
-import select from "../../assets/icon-select.svg";
-import download from "../../assets/icon-dwn.svg";
-import edit from "../../assets/icon-edit.svg";
+
+
 import HomeNav from "./homeNav";
 import { resume } from "react-dom/server";
 function Homepage() {
-  const navigate = useNavigate();
+ 
   return (
     <>
       <HomeNav />
-      <div className=" flex flex-col justify-center text-center  mt-15">
-        <div className="p-1">
-          <h1 class="font-inter text-5xl text-blue-500 lg:text-6xl font-bold">
-            Get Your Dream Job
-          </h1>
-        </div>
-        <div>
-          <h1 class="font-inter text-3xl font-medium mb-1 lg:text-4xl">
-            With My Resume Maker
-          </h1>
-        </div>
-      </div>
-      <div class="mt-6 flex justify-center ">
-        <p class="font-Inter text-center text-[13px] mx-10 md:text-9 lg:text-[0.9rem] ">
-          Get your resume noticed with our intutive builder Designed to help you
-          build a professional profile that gets result.
-        </p>
-      </div>
-      <div class="flex justify-center mt-8">
-        <button
-          onClick={() => navigate("/navigation")}
-          class="bg-blue-400 rounded-xl px-5 py-1.5  text-white cursor-pointer"
-        >
-          Create Resume
-        </button>
-      </div>
+      <Hero/>
+      <Steps/>
+      
 
-      <div class="p-5 mt-10 md:flex  md:mx-10 lg:md:mx-60 lg:mt-20 lg:justify-between">
-        <div class="relative overflow-hidden h-115 bg-blue-200 rounded-2xl group md:w-190 lg:w-[35vw]  flex justify-center items-end">
-          <img
-            src={temp7}
-            class="absolute top-25 left-6 h-[89%] 
-         opacity-95 -rotate-2
-         transition-transform duration-500 ease-out
-         group-hover:-translate-y-6 
-         group-hover:scale-110 
-         group-hover:rotate-[-6deg]"
-          />
-
-          <img
-            src={temp19}
-            class="absolute top-25 right-6 h-[89%] 
-         opacity-95 rotate-2
-         transition-transform duration-500 ease-out
-         group-hover:-translate-y-6 
-         group-hover:scale-110 
-         group-hover:rotate-6"
-          />
-
-          <img
-            src={temp18}
-            class="absolute top-19 left-1/2 -translate-x-1/2 h-[95%] 
-         z-10 shadow-xl
-         transition-transform duration-500 ease-out
-         group-hover:-translate-y-10 
-         group-hover:scale-110"
-          />
-        </div>
-        <div class="lg:ml-10">
-          <h1 class="mt-19 text-4xl font-medium md:text-4xl md:m-1">
-            Let’s build your free, job-winning
-          </h1>
-          <span class="text-blue-600 font-medium text-4xl">
-            {" "}
-            resume in 3 simple steps
-          </span>
-
-          <div class="mt-9">
-            <div class="mb-3">
-              <span class="flex items-center gap-3">
-                <div class="flex items-center justify-center">
-                  <img
-                    class="h-12.5"
-                    src={select}
-                  ></img>
-                </div>
-                <h2 class="font-medium md:text-lg">Step1: Choose desired template</h2>
-              </span>
-              <p class="text-gray-500 ml-14">
-                Select you prefered resume templates from a vide variety of
-                templates.
-              </p>
-            </div>
-
-            <div class="mb-3">
-              <span class="flex items-center gap-3">
-                <div class="flex items-center justify-center">
-                  <img
-                    class="h-14 "
-                    src={edit}
-                  ></img>
-                </div>
-                <h2 class="font-medium md:text-lg md:ml-1">Step2: Add your details</h2>
-              </span>
-              <p class="text-gray-500 ml-14">
-                Add details about your experience, education, and skills with
-                one click..
-              </p>
-            </div>
-            <span class="flex items-center gap-1">
-              <div class="flex items-center justify-center">
-                <img
-                  class="h-14 md:ml-1"
-                  src={download}
-                ></img>
-              </div>
-              <h2 class="font-medium md:text-lg">Step1: Download your resume 4 free</h2>
-            </span>
-            <p class="text-gray-500 ml-14">
-              Add details about your experience, education, and skills with one
-              click..
-            </p>
-          </div>
-        </div>
-      </div>
+      
+      
 
       <div className="mt-15 bg-linear-to-r from-[#21b1c1] via-[#1d80ea] to-[#1d8fbc] h-[80vh] p-2">
         <h2 className="text-center text-[20px] font-bold font-Inter  text-white lg:text-2xl">
@@ -140,7 +30,7 @@ function Homepage() {
           helps you create a standout resumes in minutes.{" "}
         </p>
 
-        <TemplateSlider/>
+        <TemplateSlider />
       </div>
     </>
   );
